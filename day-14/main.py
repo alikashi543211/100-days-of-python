46bf135608a38a1af1523c421f807496e1ff1fb5
unknown <kashif@softlinks.ae>
2025-12-22 15:32:05 +0500

    day 14 practise

---
+++ day-14/main.py
@@ -0,0 +1,44 @@
+a = int(input("Enter your age: "))
+
+print("Your age is:", a)
+
+# Conditional Operators
+# >, <, >=, <=, ==, !=
+# print(a > 18)
+# print(a >= 18)
+# print(a < 18)
+# print(a <= 18)
+# print(a == 18)
+# print(a != 18)
+if(a > 18):
+    print("You can drive")
+else:
+    print("You can not drive")
+    print("No")
+    print('Yay')
+
+
+applePrice = 210
+budget = 200
+
+if(applePrice <= budget):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
+
+
+num = int(input('Enter the value of num:'))
+
+if(num < 0):
+    print("Number is negative.")
+elif(num == 0):
+    print("Number is Zero.")
+elif(num == 999):
+    print("Number is Special.")
+else:
+    print("Number is positive.")
+
+
+print("I am happy now.")
+
+
