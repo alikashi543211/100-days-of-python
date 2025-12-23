313e2847471239388aa660336a0864cc47ffde8e
alikashi543211 <alikashi54321@gmail.com>
2025-12-23 05:41:05 +0500

    day 19 python

---
+++ day-18/main.py
@@ -0,0 +1,23 @@
+# for i in range(3):
+#     print(i)
+
+# i = int(input("Enter the number: "))
+# while(i <= 38):
+#     i = int(input("Enter the number: "))
+#     print(i)
+
+# print("Done with the Loop")
+
+# count = 5
+# while(count > 0):
+#     print(count)
+#     count = count - 1
+# else:
+#     print("I am inside else")
+
+i = 0
+while(True):
+    print(i)
+    i = i + 1
+    if(i > 9):
+        break
---
+++ day-19/main.py
@@ -0,0 +1,6 @@
+for i in range(12):
+    if(i == 10):
+        print("Skip iteration")
+        continue
+    print("5 X ", i, "=", 5 * i)
+    
