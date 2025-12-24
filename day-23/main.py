5dc192deb179fa66c5f8391e606b8fba6126c046
unknown <kashif@softlinks.ae>
2025-12-24 19:05:38 +0500

    day 23 half

---
+++ day-23/main.py
@@ -0,0 +1,17 @@
+l = [11, 45, 1, 2, 4, 6, 1]
+
+print(l)
+
+# l.append(7)
+# l.sort(reverse=True)
+# l.reverse()
+# print(l.index(1))
+# print(l.count(1))
+
+# m = l.copy()
+# m[0] = 0
+
+# l.insert(1, 899)
+l.extend
+
+print(l)
