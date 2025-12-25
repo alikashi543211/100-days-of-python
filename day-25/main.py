4e30611b34349b98db3705980a46d44b520b8499
alikashi543211 <alikashi54321@gmail.com>
2025-12-25 22:10:14 +0500

    day-25

---
+++ day-25/main.py
@@ -0,0 +1,20 @@
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+
+# temp = list(countries)
+# temp.append("Russia")
+# temp.pop(3)
+# temp[2] = "Finland"
+# countries = tuple(temp)
+# print(countries)
+
+# countries = ("Pakistan", "Afghanistan", "Bangladesh", "Sri Lanka")
+# countries2 = ("Vietnam", "India", "China")
+# southEastAsia = countries + countries2
+# print(southEastAsia)
+
+tuple1 = (0, 1, 2, 31, 4, 3, 1, 3, 2, 3)
+# res = tuple1.count(3)
+# res = tuple1.index(31)
+res = len(tuple1)
+print("Count of 3 in tuple1 is:", res)
+
